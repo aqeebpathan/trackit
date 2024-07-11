65e5b120cda9e8d632fe5a194482dace67f5df3c
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-07-11 17:17:54 +0530

    Frontend Initialize

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TrakIt - Your Ultimate Task Tracking Solution",
+  description: "TrakIt: Simplify task management and track progress securely.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={clsx(dmSans.className, "antialiased")}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Banner from "./_components/Banner";
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
+import LogoTicker from "./_components/LogoTicker";
+import Features from "./_components/Features";
+import ProductShowcase from "./_components/ProductShowcase";
+import FAQs from "./_components/FAQs";
+import CallToAction from "./_components/CallToAction";
+import Footer from "./_components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Navbar />
+      <Hero />
+      <LogoTicker />
+      <Features />
+      <ProductShowcase />
+      <FAQs />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
