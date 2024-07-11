725ea08393343f4bc892eeb09094271bd6e901c3
Mohmmed Aqeeb <developer.aqeeb@gmail.com>
2024-07-11 17:11:24 +0530

    Add constants

---
+++ app/_constants/index.ts
@@ -0,0 +1,109 @@
+// LogoTicker Images
+import acmeLogo from "@/assets/images/acme.png";
+import apexLogo from "@/assets/images/apex.png";
+import celestiaLogo from "@/assets/images/celestia.png";
+import echoLogo from "@/assets/images/echo.png";
+import pulseLogo from "@/assets/images/pulse.png";
+import quantumLogo from "@/assets/images/quantum.png";
+
+//
+import XIcon from "@/assets/icons/Vector.svg";
+import InstagramIcon from "@/assets/icons/instagram.svg";
+import LinkedInIcon from "@/assets/icons/linkedin.svg";
+import YoutubeIcon from "@/assets/icons/youtube.svg";
+
+export const navLinks = [
+  {
+    label: "About",
+    href: "#",
+  },
+  {
+    label: "Features",
+    href: "#",
+  },
+  {
+    label: "Updates",
+    href: "#",
+  },
+  {
+    label: "Help",
+    href: "#",
+  },
+  {
+    label: "Customers",
+    href: "#",
+  },
+];
+
+export const LogoTickerImages = [
+  {
+    src: acmeLogo,
+    alt: "Acme Logo",
+  },
+  {
+    src: apexLogo,
+    alt: "Apex Logo",
+  },
+  {
+    src: celestiaLogo,
+    alt: "Celestia Logo",
+  },
+  {
+    src: echoLogo,
+    alt: "Echo Logo",
+  },
+  {
+    src: pulseLogo,
+    alt: "Pulse Logo",
+  },
+  {
+    src: quantumLogo,
+    alt: "Quantum Logo",
+  },
+];
+
+export const features = [
+  {
+    title: "Integration ecosystem",
+    description:
+      "Enhance your productivity by connecting with your favorite tools, keeping all your essentials in one place.",
+  },
+  {
+    title: "Goal setting and tracking",
+    description:
+      "Define and track your goals, breaking down objectives into achievable tasks to keep your targets in sight.",
+  },
+  {
+    title: "Secure data encryption",
+    description:
+      "With end-to-end encryption, your data is securely stored and protected from unauthorized access.",
+  },
+];
+
+export const faqs = [
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "We accept all major credit cards, PayPal, and various digital payment methods to ensure convenience for our customers.",
+  },
+  {
+    question: "How does the pricing work for teams?",
+    answer:
+      "Team pricing is based on the number of members and the selected plan tier. We offer customized quotes tailored to your team's specific needs and size.",
+  },
+  {
+    question: "Can I change my plan later?",
+    answer:
+      "Yes, you can upgrade or downgrade your plan at any time from your account settings.",
+  },
+  {
+    question: "Is my data secure?",
+    answer:
+      "We prioritize data security by implementing robust encryption methods and conducting regular audits to safeguard your sensitive information.",
+  },
+  {
+    question: "How does the pricing work for teams?",
+    answer:
+      "Our team pricing model is designed to scale with your team size. Contact us today to receive detailed pricing options tailored to meet your specific business requirements.",
+  },
+];
